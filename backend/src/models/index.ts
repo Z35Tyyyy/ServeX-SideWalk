7e79d64b9b8bfcd5e753e97dcd20eb1cd93399ee
Kanishk Singh <kanishksingh778@gmail.com>
2026-01-22 22:23:21 +0530

    fix: changes

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,6 +1,6 @@
-export { Table, type ITable } from './Table';
-export { MenuItem, type IMenuItem } from './MenuItem';
-export { Order, type IOrder, type IOrderItem } from './Order';
-export { Payment, type IPayment } from './Payment';
-export { User, type IUser } from './User';
-export { TableSession, type ITableSession, generateSessionToken } from './TableSession';
+export { Table, type ITable } from './Table.js';
+export { MenuItem, type IMenuItem } from './MenuItem.js';
+export { Order, type IOrder, type IOrderItem } from './Order.js';
+export { Payment, type IPayment } from './Payment.js';
+export { User, type IUser } from './User.js';
+export { TableSession, type ITableSession, generateSessionToken } from './TableSession.js';
